2b0f3280cde5556a9d74ea0799803ce0cd6856c0
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-01-04 15:38:52 +0000

    register and authenticate

--- models.py
+++ models.py
@@ -29,3 +29,36 @@ class User(db.Model):
     email = db.Column(db.String(50), nullable=False)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
+
+    # start of convenience class methods
+
+    @classmethod
+    def register(cls, username, password, first_name, last_name, email):
+        """Register a user, hashing their password."""
+
+        hashed = bcrypt.generate_password_hash(password)
+        hashed_utf8 = hashed.decode("utf8")
+        user = cls(
+            username=username,
+            password=hashed_utf8,
+            first_name=first_name,
+            last_name=last_name,
+            email=email
+        )
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Validate that user exists & password is correct.
+
+        Return user if valid; else return False.
+        """
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            return user
+        else:
+            return False
